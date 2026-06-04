43b2f979303d67cee3bf4efea3deb6c280f19322
Emir Wanes Aouioua <emirwanes.aouioua@studio.unibo.it>
2026-06-04 20:13:58 +0200

    set light theme as default

---
+++ docs/_app/immutable/entry/start.DX_QotJ1.js
@@ -0,0 +1,1 @@
+import{o as e,t}from"../chunks/BQk2z-Iw.js";export{e as load_css,t as start};
--- src/lib/stores/themes.svelte.ts
+++ src/lib/stores/themes.svelte.ts
@@ -1,5 +1,5 @@
 // defaults to dark mode
-export const theme = $state({ dark: true });
+export const theme = $state({ dark: false });
 
 export const toggleDarkMode = () => {
     theme.dark = !theme.dark;
